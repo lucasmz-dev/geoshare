275df8b8de871645bbfeafc115e2c7bdb838bb0a
Jakub Valenta <jakub@jakubvalenta.cz>
2024-11-05 22:50:23 +0100

    build.gradle: fix when keystore.properties is missing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,10 @@
 import java.util.Properties
-import java.io.FileInputStream
 
-// Create a variable called keystorePropertiesFile, and initialize it to your
-// keystore.properties file, in the rootProject folder.
 val keystorePropertiesFile = rootProject.file("keystore.properties")
-
-// Initialize a new Properties() object called keystoreProperties.
 val keystoreProperties = Properties()
-
-// Load your keystore.properties file into the keystoreProperties object.
-keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
+}
 
 plugins {
     alias(libs.plugins.android.application)
