a4881a2220bb0cac94866ae56049ac5bc6357612
Jakub Valenta <jakub@jakubvalenta.cz>
2024-11-20 15:14:28 +0100

    ShareActivity: add support for VIEW intents
    Closes: #4

--- README.md
+++ README.md
@@ -67,13 +67,19 @@ make install
 
 ```shell
 adb -s emulator-5554 shell am start -W -a android.intent.action.SEND -t text/plain \
-    -e android.intent.extra.TEXT 'https://www.google.com/maps/place/Berlin,+Germany/@52.5067296,13.2599309,11z/data=12345?entry=ttu\&g_ep=678910"' \
+    -e android.intent.extra.TEXT "https://www.google.com/maps/place/Berlin,+Germany/@52.5067296,13.2599309,11z/data=12345?entry=ttu\&g_ep=678910" \
     page.ooooo.geoshare
 ```
 
 ```shell
 adb -s emulator-5554 shell am start -W -a android.intent.action.SEND -t text/plain \
     -e android.intent.extra.TEXT 'https://maps.app.goo.gl/eukZjpeYrrvX3tDw6?g_st=ac' \
+    page.ooooo.geoshare
+```
+
+```shell
+adb -s emulator-5554 shell am start -W -a android.intent.action.VIEW \
+    -d 'https://www.google.com/maps/place/Berlin,+Germany/@52.5067296,13.2599309,11z/data=12345?entry=ttu\&g_ep=678910' \
     page.ooooo.geoshare
 ```
 
--- app/src/main/java/page/ooooo/geoshare/ShareActivity.kt
+++ app/src/main/java/page/ooooo/geoshare/ShareActivity.kt
@@ -34,21 +34,29 @@ class ShareActivity : ComponentActivity() {
 
     private fun getIntentUrl(): URL? {
         val intentAction = intent.action
-        if (intentAction != Intent.ACTION_SEND) {
+        val urlString = if (intentAction == Intent.ACTION_VIEW) {
+            val intentData: Uri? = intent?.data
+            if (intentData == null) {
+                Log.w(null, "Missing intent data")
+                return null
+            }
+            intentData.toString()
+        } else if (intentAction == Intent.ACTION_SEND) {
+            val intentText = intent.getStringExtra("android.intent.extra.TEXT")
+            if (intentText == null) {
+                Log.w(null, "Missing intent extra text")
+                return null
+            }
+            val intentUrlMatch = intentUrlRegex.find(intentText)
+            if (intentUrlMatch == null) {
+                Log.w(null, "Intent extra text does not contain a URL")
+                return null
+            }
+            intentUrlMatch.value
+        } else {
             Log.w(null, "Unsupported intent action $intentAction")
             return null
         }
-        val intentText = intent.getStringExtra("android.intent.extra.TEXT")
-        if (intentText == null) {
-            Log.w(null, "Missing intent extra text")
-            return null
-        }
-        val intentUrlMatch = intentUrlRegex.find(intentText)
-        if (intentUrlMatch == null) {
-            Log.w(null, "Intent extra text does not contain a URL")
-            return null
-        }
-        val urlString = intentUrlMatch.value
         return try {
             URL(urlString)
         } catch (_: MalformedURLException) {
