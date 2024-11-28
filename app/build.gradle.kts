8b478d43711da0e175aac6cb7e20d507df12ca86
Jakub Valenta <jakub@jakubvalenta.cz>
2024-11-28 14:32:53 +0100

    v1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "page.ooooo.geoshare"
         minSdk = 25
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.4.0"
+        versionCode = 8
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
